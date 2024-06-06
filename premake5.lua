d93c3852f2380920c8958074ab69e41027ed54d0
solfrak <laporte.samuel2000@gmail.com>
2024-06-06 19:28:02 -0400

    add premake file

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+﻿project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imgui.cpp",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui_demo.cpp",
+        "imgui_draw.cpp",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_glfw.cpp"
+	}
+
+    includedirs
+       {
+           "./",
+           "backends",
+           "../glfw/include"
+       }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
